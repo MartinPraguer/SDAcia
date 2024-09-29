56cfa87cacc6c769b912d50ba08fa519763ea22b
Martin Praguer <martin.praguer@gmail.com>
2024-09-29 13:32:23 +0200

    úprava souborů a přejmenování složky hollymovies na správný název SDAcia

--- SDAcia/urls.py
+++ SDAcia/urls.py
@@ -1,4 +1,4 @@
-"""hollymovies URL Configuration
+"""SDAcia URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
--- SDAcia/wsgi.py
+++ SDAcia/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hollymovies project.
+WSGI config for SDAcia project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hollymovies.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SDAcia.settings')
 
 application = get_wsgi_application()
